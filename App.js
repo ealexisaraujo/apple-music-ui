c3543f920792f4f5487b7d7e163521c6f2125593
Alexis Araujo <ala1289@gmail.com>
2018-07-08 15:09:19 -0500

    Fixes issues ui

--- App.js
+++ App.js
@@ -1,67 +1,81 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet,Dimensions, Animated,PanResponder,ScrollView, Image, Slider} from 'react-native';
-import {Ionicons} from '@expo/vector-icons'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Animated,
+  PanResponder,
+  ScrollView,
+  Image,
+  Slider
+} from "react-native";
+import { Ionicons } from '@expo/vector-icons'
 
 const SCREEN_HEIGHT = Dimensions.get('window').height
 const SCREEN_WIDTH = Dimensions.get('window').width
 
-class componentName extends Component {
+class AppleMusicUI extends Component {
 
   state = {
     isScrollEnabled: false
   }
+  componentWillMount() {
 
-componentWillMount() {
+    this.scrollOffset = 0
 
-  this.scrollOffset = 0
+    this.animation = new Animated.ValueXY({ x: 0, y: SCREEN_HEIGHT - 90 })
 
-  this.animation = new Animated.ValueXY({x: 0, y: SCREEN_HEIGHT - 90})
+    this.panResponder = PanResponder.create({
 
-  this.PanResponder = PanResponder.create({
-    onMoveShouldSetPanResponder: (evt, gestureState) => {
+      onMoveShouldSetPanResponder: (evt, gestureState) => {
 
-      if ((this.state.isScrollEnabled && this.scrollOffset <= 0 && gestureState.dy > 0) || !this.state.isScrollEnabled && gestureState.dy < 0) {
-        return true
-      } else {
-        return false
+        if ((this.state.isScrollEnabled && this.scrollOffset <= 0 && gestureState.dy > 0) || !this.state.isScrollEnabled && gestureState.dy < 0) {
+          return true
+        } else {
+          return false
+        }
+      },
+      onPanResponderGrant: (evt, gestureState) => {
+        this.animation.extractOffset()
+      },
+      onPanResponderMove: (evt, gestureState) => {
+
+        this.animation.setValue({ x: 0, y: gestureState.dy })
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+
+        if (gestureState.moveY > SCREEN_HEIGHT - 120) {
+          Animated.spring(this.animation.y, {
+            toValue: 0,
+            tension: 1
+          }).start()
+        }
+        else if (gestureState.moveY < 120) {
+          Animated.spring(this.animation.y, {
+            toValue: 0,
+            tension: 1
+          }).start()
+        }
+        else if (gestureState.dy < 0) {
+          this.setState({ isScrollEnabled: true })
+
+          Animated.spring(this.animation.y, {
+            toValue: -SCREEN_HEIGHT + 120,
+            tension: 1
+          }).start()
+        }
+        else if (gestureState.dy > 0) {
+          this.setState({ isScrollEnabled: false })
+          Animated.spring(this.animation.y, {
+            toValue: SCREEN_HEIGHT - 120,
+            tension: 1
+          }).start()
+        }
       }
-    },
-    onPanResponderGrant:(evt,gestureState)=>{
-      this.animation.extractOffset()
-    },
-    onPanResponderMove:(evt,gestureState)=>{
-      this.animation.setValue({x:0, y: gestureState.dy})
-    },
-    onPanResponderRelease:(evt,gestureState)=>{
-      if (gestureState.moveY > SCREEN_HEIGHT - 120) {
-        Animated.spring(this.animation.y, {
-          toValue: 0,
-          tension: 1
-        }).start()
-      }
-      else if (gestureState.moveY < 120) {
-        Animated.spring(this.animation.y, {
-          toValue: 0,
-          tension: 1
-        }).start()
-      }
-      else if (gestureState.dy < 0) {
-        this.setState({isScrollEnabled: true})
-        Animated.spring(this.animation.y,{
-          toValue: -SCREEN_HEIGHT + 120,
-          tension:1
-        }).start()
-      }
-      else if (gestureState.dy < 0) {
-        this.setState({isScrollEnabled: false})
-        Animated.spring(this.animation.y,{
-          toValue: SCREEN_HEIGHT - 120,
-          tension:1
-        }).start()
-      }
-    }
-  })
-}
+
+    })
+  }
 
   render() {
 
@@ -70,18 +84,18 @@ componentWillMount() {
     }
 
     animatedImageHeight = this.animation.y.interpolate({
-      inputRange:[0, SCREEN_HEIGHT-90],
-      outputRange:[200,32],
+      inputRange: [0, SCREEN_HEIGHT - 90],
+      outputRange: [200, 32],
       extrapolate: "clamp"
     })
-    animatedSongTittleOpacity = this.animation.y.interpolate({
-      inputRange:[0, SCREEN_HEIGHT - 500,SCREEN_HEIGHT-90],
-      outputRange:[0,0,1],
+    animatedSongTitleOpacity = this.animation.y.interpolate({
+      inputRange: [0, SCREEN_HEIGHT - 500, SCREEN_HEIGHT - 90],
+      outputRange: [0, 0, 1],
       extrapolate: "clamp"
     })
     animatedImageMarginLeft = this.animation.y.interpolate({
-      inputRange:[0,SCREEN_HEIGHT-90],
-      outputRange:[SCREEN_WIDTH/2 - 100, 10],
+      inputRange: [0, SCREEN_HEIGHT - 90],
+      outputRange: [SCREEN_WIDTH / 2 - 100, 10],
       extrapolate: "clamp"
     })
     animatedHeaderHeight = this.animation.y.interpolate({
@@ -89,9 +103,9 @@ componentWillMount() {
       outputRange: [SCREEN_HEIGHT / 2, 90],
       extrapolate: "clamp"
     })
-    animatedSongDetailOpacity = this.animation.y.interpolate({
-      inputRange:[0, SCREEN_HEIGHT - 500,SCREEN_HEIGHT-90],
-      outputRange:[1,0,0],
+    animatedSongDetailsOpacity = this.animation.y.interpolate({
+      inputRange: [0, SCREEN_HEIGHT - 500, SCREEN_HEIGHT - 90],
+      outputRange: [1, 0, 0],
       extrapolate: "clamp"
     })
     animatedBackgroundColor = this.animation.y.interpolate({
@@ -99,44 +113,39 @@ componentWillMount() {
       outputRange: ['rgba(0,0,0,0.5)', 'white'],
       extrapolate: "clamp"
     })
-
     return (
-      <Animated.View style={{flex:1, backgroundColor: animatedBackgroundColor,}}>
-        <Animated.View 
-        {... this.PanResponder.panHandlers}
-        style={[animatedHeight,{position: 'absolute',
-          left: 0,
-          right: 0,
-          zIndex: 10,
-          backgroundColor: 'white',
-          height: SCREEN_HEIGHT}]
-        }> 
-        <ScrollView
+      <Animated.View style={{ flex: 1, backgroundColor: animatedBackgroundColor }}>
+        <Animated.View
+          {... this.panResponder.panHandlers}
+          style={[animatedHeight, { position: 'absolute', left: 0, right: 0, zIndex: 10, backgroundColor: 'white', height: SCREEN_HEIGHT }]}
+
+        >
+          <ScrollView
             scrollEnabled={this.state.isScrollEnabled}
             scrollEventThrottle={16}
             onScroll={event => {
               this.scrollOffset = event.nativeEvent.contentOffset.y
             }}
           >
-        <Animated.View
-        style={{height: animatedHeaderHeight, borderTopWidth: 1, borderTopColor: '#ebe5e5', flexDirection: 'row', alignItems: 'center',
-        }}>
-          <View style={{flex: 4, flexDirection:'row', alignItems:'center'}}>
-            <Animated.View style={{height: animatedImageHeight, width: animatedImageHeight, marginLeft: animatedImageMarginLeft}}>
-              <Image style={{flex:1, width: null, height: null}} 
-              source={require('./assets/Hotelcalifornia.jpg')}/>
+            <Animated.View
+              style={{ height: animatedHeaderHeight, borderTopWidth: 1, borderTopColor: '#ebe5e5', flexDirection: 'row', alignItems: 'center' }}
+            >
+              <View style={{ flex: 4, flexDirection: 'row', alignItems: 'center' }}>
+                <Animated.View style={{ height: animatedImageHeight, width: animatedImageHeight, marginLeft: animatedImageMarginLeft }}>
+                  <Image style={{ flex: 1, width: null, height: null }}
+                    source={require('./assets/Hotelcalifornia.jpg')} />
+                </Animated.View>
+                <Animated.Text style={{ opacity: animatedSongTitleOpacity, fontSize: 18, paddingLeft: 10 }}>Hotel California(Live)</Animated.Text>
+              </View>
+              <Animated.View style={{ opacity: animatedSongTitleOpacity, flex: 1, flexDirection: 'row', justifyContent: 'space-around' }}>
+                <Ionicons name="md-pause" size={32} />
+                <Ionicons name="md-play" size={32} />
+              </Animated.View>
             </Animated.View>
-            <Animated.Text style={{opacity: animatedSongTittleOpacity, fontSize: 18, paddingLeft: 10,}}>
-            Hotel California(Live)
-            </Animated.Text>
-            </View>
-            <Animated.View style={{opacity: animatedSongTittleOpacity, flex: 1,flexDirection: 'row', justifyContent: 'space-around'}}>
-              <Ionicons name="md-pause" size={32}/>
-              <Ionicons name="md-play" size={32}/>
-            </Animated.View>
-        </Animated.View>
-          <Animated.View style={{height:animatedHeaderHeight, opacity: animatedSongDetailOpacity}}>
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end' }}>
+
+            <Animated.View style={{ height: animatedHeaderHeight, opacity: animatedSongDetailsOpacity }}>
+
+              <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end' }}>
                 <Text style={{ fontWeight: 'bold', fontSize: 22 }}>Hotel California (Live)</Text>
                 <Text style={{ fontSize: 18, color: '#fa95ed' }}>Eagles - Hell Freezes Over</Text>
               </View>
@@ -148,33 +157,34 @@ componentWillMount() {
                   minimumValue={18}
                   maximumValue={71}
                   value={18}
+
                 />
               </View>
-            <View style={{ flex: 2, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
+
+              <View style={{ flex: 2, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
                 <Ionicons name="md-rewind" size={40} />
                 <Ionicons name="md-pause" size={50} />
                 <Ionicons name="md-fastforward" size={40} />
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20, paddingBottom: 20 }}>
+              </View>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20, paddingBottom: 20 }}>
                 <Ionicons name="md-add" size={32} style={{ color: '#fa95ed' }} />
                 <Ionicons name="md-more" size={32} style={{ color: '#fa95ed' }} />
               </View>
-          </Animated.View>
-          <View style={{ height: 1000 }} />
+            </Animated.View>
+            <View style={{ height: 1000 }} />
           </ScrollView>
         </Animated.View>
+
       </Animated.View>
     );
   }
 }
-
-export default componentName;
+export default AppleMusicUI;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   }
 });
-
